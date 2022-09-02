3d4c7ed30daaf66cca92340d5e6cfac207b8a40d
ChristianChidi Echere <MM-20-603@tptlive.ee>
2022-09-02 15:42:51 +0300

    add list example

--- add.php
+++ add.php
@@ -1,7 +1,20 @@
 <?php
-var_dump($_POST['newitem']);
-$file = fopen('list.txt', 'a');
-fwrite($file, $_POST['newitem'] . "\n");
-fclose($file);
+if(trim($_POST['title']) && trim($_POST['body'])) {
+    if(file_exists('posts.json')){
+    $json = file_get_contents('posts.json');
+    $posts = json_decode($json, true);
+}   else {
+    $posts = [];
+}
+    $posts[] = [
+        'title' => $_POST['title'], 
+        'body' => $_POST['body']
+];
+    $json = json_encode($posts);
+    $file = fopen('list.txt', 'w');
+    fwrite($file, $json);
+    fclose($file);
+}
+
 header('Location: /index.php');
 die();  
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
 <?php
-$content = file_get_contents('list.txt');
-$items = explode("\n", $content);
-array_pop($items);
-foreach($items as $item){
-    var_dump($item);
-}
+$json = file_get_contents('posts.json');
+$posts = json_decode($json, true);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,21 +9,34 @@ foreach($items as $item){
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Ahaaa</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
     </head>
     <body>
-        <form action="/add.php" method="POST">
-            <input type="text" name="newitem">
-            <input type="submit" value="Add">
-        </form>
-        <ul>
-            <?php
+        <div>
+            <a class="button is-primary" href="/new.php">New whatever</a>
+                <ul>
+                    <?php
 
-                foreach($items as $item):
-            ?>
-            <li>
-                <?=$item?>
-            </li>
-            <?php endforeach; ?>
-        </ul>
+                        foreach($items as $item):
+                    ?>
+                    <div class="card">
+                        <header class="card-header">
+                            <p class="card-header-title">
+                            Component
+                                <?=$post['title']?>
+                        </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content"> 
+                                <?=$post['body']?>
+                            </div>
+                        </div>
+                        <footer class="card-footer">
+                            <a href="#" class="card-footer-item">Delete</a>
+                        </footer>
+                        </div>
+                    <?php endforeach; ?>
+                </ul>
+        </div>
     </body>
 </html>
